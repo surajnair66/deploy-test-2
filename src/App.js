de5b68111d3cb17bee9febed20389c91be0496d6
Suraj Nair <suraj.n@hubspire.com>
2023-01-23 01:29:44 +0530

    theme changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AppBarTop from './Components/Layout/index'
 
 function App() {
   return (
     <div className="App">
+    <AppBarTop />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Components/Layout/index.js
@@ -0,0 +1,9 @@
+import { AppBar} from '@mui/material'
+
+ const AppBarTop = () => {
+    return ( <div>
+        <AppBar position='static' />
+    </div> );
+}
+ 
+export default AppBarTop;
